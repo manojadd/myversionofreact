35ca88d721744b3ce539b523a66f30f5bd2387a5
manoj <human.hb@gmail.com>
2017-01-11 11:56:12 +0530

    created the basic layout without responsiveness

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,7 +1,28 @@
+var injectTapEventPlugin = require("react-tap-event-plugin");
+var ReactGridLayout = require('react-grid-layout').Responsive;
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1>helddddddddddddddddddddddddddddfjklllooosdljfsdlkfj!</h1>,
-  document.getElementById('root')
-);	
+class MyFirstGrid extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      layout : [
+      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
+      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
+      {i: 'c', x: 4, y: 0, w: 1, h: 2}
+    ]};
+  }
+  render(){
+    return (
+      
+      <ReactGridLayout className="layout"  layout={this.state.layout} cols={12} rowHeight={30} width={1200}>
+        <div key="a" >a</div>
+        <div key="b" >b</div>
+        <div key="c" >c</div>
+      </ReactGridLayout>
+    );
+  }
+};
+
+ReactDOM.render(<MyFirstGrid />, document.getElementById('root'));
